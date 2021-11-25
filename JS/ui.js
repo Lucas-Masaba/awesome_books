3b4ab4fecf5882e9c30ea47c848db071b1e46c66
Khusiima Luke Masaba <masabaluke@gmail.com>
2021-11-25 18:55:58 +0300

    add form validation

--- JS/ui.js
+++ JS/ui.js
@@ -4,7 +4,8 @@ class UI {
   }
 
   static validateForm() {
-
+    const inputFields = [...document.querySelectorAll("input")]
+    inputFields.every(inputField => inputField.trim().length > 2)
   }
   static addBook() {
 
