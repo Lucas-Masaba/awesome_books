ae447dbb3cbf367ddb1eab6a8f7f076e254aace9
Khusiima Luke Masaba <masabaluke@gmail.com>
2021-11-25 19:27:55 +0300

    add to ui.js

--- JS/ui.js
+++ JS/ui.js
@@ -1,6 +1,8 @@
+
 class UI {
-  static displayBooks() {
-    const addButton = document.getElementById("add");
+    
+    static displayBooks() {
+        const addButton = document.getElementById("add");
         const booksUL = document.getElementById("books");
         const allBooks = Storage.allBooks;
 
@@ -20,32 +22,66 @@ class UI {
         addButton.addEventListener("click", UI.addBook)
 
         booksUL.addEventListener( "click", (event) => UI.remove(event) );
-  }
-
-  static validateForm(input) {
-    const inputFields = [...document.querySelectorAll(input)];
-    inputFields.every((inputField) => inputField.trim().length > 2);
-  }
-
-  static addBook(event) {
-    event.preventDefault();
-    if (UI.validateForm('form input')) {
-      const form = document.querySelector('form');
-
-      const id = Storage.books.length + 1;
-      const newBook = {
-        id,
-        title: form.title.value,
-        author: form.author.value,
-      };
-
-      Book.save(newBook);
-      UI.appendBook(newBook);
-      UI.resetForm();
     }
-  }
 
-  static removeBook() {
+    static addBook(event) {
+        
+        event.preventDefault();
 
-  }
+        if ( UI.formIsValid("form input") ) {
+
+            const form = document.querySelector("form");
+
+            const id = Storage.allBooks.length + 1;
+            const newBook = { 
+                id, 
+                title: form.title.value, 
+                author: form.author.value 
+            }
+    
+            Book.save(newBook)
+            UI.appendBook(newBook)
+            UI.resetForm()
+        }
+    }
+
+    static formIsValid(form) {
+        const inputs  = [...document.querySelectorAll(form)];
+        return inputs.every( input => input.value.trim().length > 2)
+    }
+
+    static appendBook(newBook) {
+        
+        const booksUL = document.getElementById("books");
+        const newBookToAppend = document.createElement("li");
+
+        newBookToAppend.innerHTML = `
+            <h3>${newBook.title}</h3>
+            <p>Author: ${newBook.author}</p>
+            <button data-id="${newBook.id}" type="button">Remove</button>
+            <hr>
+        `;
+
+        booksUL.appendChild(newBookToAppend);
+    }
+
+    static remove(event) {
+
+        if (event.target.tagName === "BUTTON") {
+            
+            const bookId = event.target.getAttribute("data-id");
+            Book.remove(bookId);
+
+            const listItem =  event.target.parentElement;
+            listItem.remove();
+
+        }
+
+    }
+
+    static resetForm() {
+        const inputs = [...document.querySelectorAll("form input")]
+        inputs.forEach( input => input.value = "" )
+    }
+
 }
