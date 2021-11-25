a6fef2a94d54cb68f0fbf80e4e16364e129e5348
Khusiima Luke Masaba <masabaluke@gmail.com>
2021-11-25 19:33:11 +0300

    git add content to book.js

---
+++ JS/book.js
@@ -0,0 +1,17 @@
+class Book {
+
+    static save(book) {
+        const existingBooks = JSON.parse( localStorage.getItem("mybooks") )
+        const updatedBooks = [...existingBooks, book]
+
+        localStorage.setItem("mybooks", JSON.stringify(updatedBooks))
+
+    }
+
+    static remove(bookId) {
+        const existingBooks = JSON.parse( localStorage.getItem("mybooks") )
+        const afterRemoved = existingBooks.filter( book => book.id != bookId )
+        console.log(bookId, existingBooks, afterRemoved  )
+        localStorage.setItem("mybooks", JSON.stringify(afterRemoved))
+    }
+}
